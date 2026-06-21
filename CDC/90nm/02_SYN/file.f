af165172a1b5c69058e40919cf082ee7ea89822b
akira2963753 <harry2963753@gmail.com>
2026-06-21 16:57:05 +0800

    CDC Training Finish

---
+++ CDC/90nm/02_SYN/file.f
@@ -0,0 +1,8 @@
++incdir+./
+-sverilog ../01_RTL/fifomem.v
+-sverilog ../01_RTL/sync_r2w.v
+-sverilog ../01_RTL/sync_w2r.v
+-sverilog ../01_RTL/wptr_full.v
+-sverilog ../01_RTL/rptr_empty.v
+-sverilog ../01_RTL/AFIFO.v
+-sverilog ../01_RTL/CDC.v
